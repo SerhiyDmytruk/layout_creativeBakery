11b5c42f30ae9ae24755b61dcbd99cc32121c949
Serhii Dmytruk <dmitruksl92@gmail.com>
2025-12-13 18:32:41 +0200

    feat: added logic for mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//  Header
+const toggle = document.querySelectorAll('.header__toggle');
+const body = document.body;
+const menuLink = document.querySelectorAll('.header__menu a');
+
+function headerToggle() {
+  if (body.classList.contains('_menu-open')) {
+    body.classList.remove('_menu-open');
+  } else {
+    body.classList.add('_menu-open');
+  }
+}
+
+toggle.forEach((item) => {
+  item.addEventListener('click', headerToggle);
+});
+
+menuLink.forEach((item) => {
+  item.addEventListener('click', () => {
+     body.classList.remove('_menu-open');
+  });
+});
